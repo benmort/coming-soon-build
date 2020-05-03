5c77606acb13e5714ad6a697a328d4bf519424b4
Benjamin Mort <benm@getup.org.au>
2020-05-03 19:44:25 +1000

    init

---
+++ precache-manifest.6bccdb15d12b4d8cde7e77ac53748884.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5ab3fbd2f45428c2f7f74a2723cda9",
+    "url": "/coming-soon/index.html"
+  },
+  {
+    "revision": "e3d5f7718929c0a1706b",
+    "url": "/coming-soon/static/css/main.e6163644.chunk.css"
+  },
+  {
+    "revision": "ea38a90e1dbe62429cc3",
+    "url": "/coming-soon/static/js/2.568b4bfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coming-soon/static/js/2.568b4bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d5f7718929c0a1706b",
+    "url": "/coming-soon/static/js/main.25005530.chunk.js"
+  },
+  {
+    "revision": "844233a8b8783b4fdafe",
+    "url": "/coming-soon/static/js/runtime-main.9c3e133f.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/coming-soon/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/coming-soon/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/coming-soon/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/coming-soon/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/coming-soon/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/coming-soon/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/coming-soon/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/coming-soon/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "04f1d8728b781908558c8f1442c665ed",
+    "url": "/coming-soon/static/media/insta.04f1d872.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/coming-soon/static/media/twitter.a50716dd.svg"
+  }
+]);
